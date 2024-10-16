7816f84de784d170ff39e12cd8fa62527f58bd8d
Cedric Melancon <cedric.melancon.1@ens.etsmtl.ca>
2024-10-16 14:35:16 -0400

    WIP

--- src/p3at_deepvio/p3at_deepvio/p3at_deepvio.py
+++ src/p3at_deepvio/p3at_deepvio/p3at_deepvio.py
@@ -60,8 +60,8 @@ class P3atDeepvio(Node):
         self._last_camera_data = None
         self._last_stamp = None
 
-        self._img_seq = torch.zeros([self._odometry_model.clip_length, 1, 81920]).to('cuda:0')
-        #self._img_seq = torch.zeros([self._odometry_model.clip_length, 1, 3, 2, 480, 640]).to('cuda:0')
+        #self._img_seq = torch.zeros([self._odometry_model.clip_length, 1, 81920]).to('cuda:0')
+        self._img_seq = torch.zeros([self._odometry_model.clip_length, 1, 3, 2, 480, 640]).to('cuda:0')
 
         self._imu_seq = collections.deque(maxlen=self._odometry_model.clip_length)
         self._monitoring_data = collections.deque()
@@ -102,7 +102,7 @@ class P3atDeepvio(Node):
         camera_data = self.image_to_tensor(camera_data, height, width)
 
         if last_camera_data is not None:
-            #self._model_lock.acquire()
+            self._model_lock.acquire()
             start_time = time.perf_counter()
             last_camera_data = self.image_to_tensor(last_camera_data, height, width)
 
@@ -113,13 +113,13 @@ class P3atDeepvio(Node):
             img_pair = torch.from_numpy(img_pair).type(torch.FloatTensor).to('cuda:0')
 
             #print(img_pair.shape)
-            feature_data = self._odometry_model.forward_flownet(img_pair)
-            flownet_time = (time.perf_counter() - start_time)
-            #flownet_time = 0.0
+            #feature_data = self._odometry_model.forward_flownet(img_pair)
+            #flownet_time = (time.perf_counter() - start_time)
+            flownet_time = 0.0
 
-            self._model_lock.acquire()
-            self.push_to_tensor_alternative(self._img_seq, feature_data)
-            #self.push_to_tensor_alternative(self._img_seq, img_pair)
+            #self._model_lock.acquire()
+            #self.push_to_tensor_alternative(self._img_seq, feature_data)
+            self.push_to_tensor_alternative(self._img_seq, img_pair)
 
             odometry = None
 
@@ -135,7 +135,7 @@ class P3atDeepvio(Node):
                 prev_beliefs = self.beliefs
 
                 with torch.no_grad():
-                    self.beliefs, odometry, timing = self._odometry_model.forward(self._img_seq, imu_seq, prev_beliefs)
+                    self.beliefs, odometry, timing = self._odometry_model.forward_full(self._img_seq, imu_seq, prev_beliefs)
 
                 self._monitoring_lock.acquire()
                 self._monitoring_data.append(
