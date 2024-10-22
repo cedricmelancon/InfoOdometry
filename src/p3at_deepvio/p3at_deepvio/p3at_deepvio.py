20de7f7c88e567ad5d143e4c3182a387dd84082b
Cedric Melancon <cedric.melancon@gmail.com>
2024-10-22 10:13:31 -0400

    WIP

--- src/p3at_deepvio/p3at_deepvio/p3at_deepvio.py
+++ src/p3at_deepvio/p3at_deepvio/p3at_deepvio.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Imu
 import cv2
 from PIL import Image as Img
 from threading import Lock
+import threading
 import numpy as np
 import torch
 import collections
@@ -27,6 +28,8 @@ class P3atDeepvio(Node):
     def __init__(self):
         super().__init__('P3atDeepvio')
         #self._service_manager = ServiceManager()
+
+        self._thread_local = threading.local()
 
         self._monitor = Monitor(cuda_enabled=True)
         self.beliefs = None
@@ -99,8 +102,10 @@ class P3atDeepvio(Node):
         gpu_perc = smi["05_gpu_smi/gpu_0_fb_free_MiB"]
         gpu_temp = smi["05_gpu_smi/gpu_0_temp_in_C"]
         gpu_power = smi["05_gpu_smi/gpu_0_power_in_W"]
+        gpu_util = smi["05_gpu_smi/gpu_0_gpu_util_in_percent"]
+        mem_util = smi["05_gpu_smi/gpu_0_mem_util_in_percent"]
 
-        data = np.array([cpu, ram_avail, ram_used, ram_perc, gpu_avail, gpu_used, gpu_perc, gpu_temp, gpu_power])
+        data = np.array([time.perf_counter(), cpu, ram_avail, ram_used, ram_perc, gpu_avail, gpu_used, gpu_perc, gpu_temp, gpu_power, gpu_util, mem_util])
         self.system_csv_writer.writerow(data)
         self._sys_mon_lock.release()
 
@@ -122,8 +127,12 @@ class P3atDeepvio(Node):
     def push_to_tensor_alternative(tensor, x):
         return torch.cat((tensor[1:7], x))
 
-    def process_data(self, camera_data, last_camera_data, imu_data, height, width, current_stamp):
+    def process_data(self, frame_nb, camera_data, last_camera_data, imu_data, height, width, current_stamp):
         camera_data = self.image_to_tensor(camera_data, height, width)
+        self._thread_local.frame_nb = frame_nb
+
+        imu_seq = torch.from_numpy(imu_data).type(torch.FloatTensor).to('cuda:0')
+        self._thread_local.imu_seq = imu_seq.unsqueeze(0)
 
         if last_camera_data is not None:
             self._flownet_lock.acquire()
@@ -134,31 +143,28 @@ class P3atDeepvio(Node):
             img_pair = np.array(img_pair).transpose(3, 0, 1, 2)
             img_pair = np.expand_dims(img_pair, axis=0)
             img_pair = np.expand_dims(img_pair, axis=0)
-            img_pair = torch.from_numpy(img_pair).type(torch.FloatTensor).to('cuda:0')
+            self._thread_local.img_pair = torch.from_numpy(img_pair).type(torch.FloatTensor).to('cuda:0')
 
-            feature_data = self._odometry_model.forward_flownet(img_pair)
+            self._thread_local.feature_data = self._odometry_model.forward_flownet(self._thread_local.img_pair)
 
-            flownet_time = (time.perf_counter() - start_time)
+            self._thread_local.flownet_time = (time.perf_counter() - start_time)
             self._flownet_lock.release()
 
             self._model_lock.acquire()
-            clone_feature = torch.clone(feature_data)
             
             if self.beliefs is None:
                 self.beliefs = torch.rand(1, self.args.belief_size, device=self.args.device)
 
-            imu_seq = torch.from_numpy(imu_data).type(torch.FloatTensor).to('cuda:0')
-            imu_seq = imu_seq.unsqueeze(0)
+            
             prev_beliefs = self.beliefs
 
             with torch.no_grad():
-                self.beliefs, odometry, timing = self._odometry_model.step(clone_feature, imu_seq, prev_beliefs)
+                self.beliefs, odometry, timing = self._odometry_model.step(self._thread_local.feature_data, self._thread_local.imu_seq, prev_beliefs)
 
             self._model_lock.release()
 
             self._monitoring_lock.acquire()
-            self._monitoring_data.append(
-                np.array([self.frame_nb, flownet_time] + timing))
+            self._monitoring_data.append(np.array([time.perf_counter(), self._thread_local.frame_nb, self._thread_local.flownet_time] + timing))
             self._monitoring_lock.release()
 
             if odometry is not None:
@@ -205,16 +211,20 @@ class P3atDeepvio(Node):
         imu_data = np.array(list(self._imu_data), copy=True)
         self._imu_lock.release()
 
-        if self.frame_nb < self.skip_frame:
+        self._camera_lock.acquire()
+        frame_nb = self.frame_nb
+        self._camera_lock.release()
+        if frame_nb < self.skip_frame:
             self.frame_nb += 1
             return
 
-        self.frame_nb += 1
         self._camera_lock.acquire()
+        self.frame_nb += 1
         camera_data = np.array(list(msg.data), dtype=np.uint8, copy=True)
         self._camera_lock.release()
         last_camera_data = np.array(self._last_camera_data, copy=True) if self._last_camera_data is not None else None
-        self.process_data(camera_data,
+        self.process_data(frame_nb,
+                          camera_data,
                           last_camera_data,
                           imu_data,
                           msg.height,
